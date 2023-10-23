509ba1457b88d8d0171d50d32591c6e770aeb0e9
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-10-23 04:18:59 -0500

    update CI

--- skywriter/i3_to_json.py
+++ skywriter/i3_to_json.py
@@ -14,8 +14,9 @@ from wipac_dev_tools import logging_tools
 from skywriter import suppress_warnings  # type: ignore[import] # noqa: F401
 
 # try old-style import for CI
-# from icecube.icetray import I3Tray  # type: ignore[import]
-from I3Tray import I3Tray  # type: ignore[import]
+from icecube.icetray import I3Tray  # type: ignore[import]
+
+# from I3Tray import I3Tray  # type: ignore[import]
 
 
 from icecube import (  # type: ignore[import] # noqa: F401
