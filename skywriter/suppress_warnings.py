51af5ce51ef48bccd9023eb0ee09e84e8c02632f
Massimiliano Lincetto <m.lincetto@gmail.com>
2023-10-23 08:35:20 -0500

    remove suppress_warnings.py

--- skywriter/suppress_warnings.py
+++
@@ -1,6 +0,0 @@
-# temporary workaround for https://github.com/icecube/icetray/issues/3112
-import warnings
-
-warnings.filterwarnings(
-    "ignore", ".*already registered; second conversion method ignored.", RuntimeWarning
-)
